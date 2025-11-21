53546eccacdf1e7aa8bd651f5155bef6c1e3f008
szymcr1_cat <Szymczak_Richard@cat.com>
2025-11-21 16:26:26 -0500

    custom javascript action

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,10 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('child_process').exec;
+
+
+function run() {
+    console.log("Deploying to AWS S3...");
+}
+
+run();
